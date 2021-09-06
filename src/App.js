79e01888f65151463ba87627597c336c1e55687a
sean Niesen <seanniesen@gmail.com>
2021-09-06 12:42:47 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import Row from "./components/Row";
+import requests from "./requests";
+import Banner from "./components/Banner";
+import Nav from "./components/Nav";
+import "./App.css";
 
 function App() {
+  const hidePreviews = (movie) => {
+    console.log(movie);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        hidePreviews={hidePreviews}
+        isLargeRow
+      />
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+        hidePreviews={hidePreviews}
+      />
+      <Row
+        title="Top Rated"
+        fetchUrl={requests.fetchTopRated}
+        hidePreviews={hidePreviews}
+      />
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+        hidePreviews={hidePreviews}
+      />
+      <Row
+        title="Comedy Movies"
+        fetchUrl={requests.fetchComedyMovies}
+        hidePreviews={hidePreviews}
+      />
+      <Row
+        title="Horror Movies"
+        fetchUrl={requests.fetchHorrorMovies}
+        hidePreviews={hidePreviews}
+      />
+      <Row
+        title="Romance Movies"
+        fetchUrl={requests.fetchRomanceMovies}
+        hidePreviews={hidePreviews}
+      />
+      <Row
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+        hidePreviews={hidePreviews}
+      />
     </div>
   );
 }
